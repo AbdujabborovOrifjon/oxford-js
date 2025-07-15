4d56dd6b7d02eaa6b1b36a2b228867c9769b5f14
AbdujabborovOrifjon <abdujabborovorifjon0@gmail.com>
2025-07-15 13:39:35 +0500

    change

---
+++ homeworks/14-dars/index.js
@@ -0,0 +1,42 @@
+// // 1 savol 
+// let fruits = ["apple", "pear", "orange"];
+
+// let shoppingcart = fruits;
+
+// shoppingcart.push("banan")
+
+// console.log(shoppingcart.length); // natija 4 chiqadi chunki shoppingcart  fruits ga boglangan
+// // keyin ularni length 4 chiqadi bularga banan ham qoshilgan
+
+
+
+
+// // 3 savol
+
+
+// let arr  = ["a", "b"]
+
+// arr.push(function () {
+//     alert(this)
+// })
+ 
+// arr[2]()  // natija a, b, function() {alert(this)} chiqadi
+// // chunki bu yerda this ni qiymati arr ga teng bo'ladi va arr ni qiymati chiqadi
+
+
+
+
+// 2 savol
+
+
+// let style = ["Jazz", "Blues"]
+
+// style.push("Rock-n-Roll")
+
+// style[1] = "Classics";
+// style.shift()
+
+// style.unshift("Rap", "Reggae")
+
+// console.log(style);
+
---
+++ lessons/13-dars/index.js
@@ -0,0 +1,161 @@
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9,]
+// let yigindi = 0
+// let toqson = 0
+
+// for (let value of arr) {
+//     if (value % 2 === 0) {
+//         console.log("juft son");
+//           yigindi = yigindi + value
+//     }else{
+//         console.log("toq son");
+//           toqson = toqson + value
+
+//     }
+
+
+// }
+
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+// for(let value of arr){
+//     console.log(value);
+    
+// }
+
+// for(let value in arr){
+//     console.log(value);
+    
+// }
+
+
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+// for (let a = 0; a < array.length; a++) {
+
+// }
+// if (arr % 2 === 19) {
+//     console.log("tub sonlar");
+
+// }
+
+// console.log(arr);
+
+// ================================================================================================
+
+
+// for (let num = 2; num <= 20; num++) {
+//   let tub = true;
+
+//   for (let i = 2; i < num; i++) {
+//     if (num % i === 0) {
+//       tub = false;
+//       break;
+//     }
+//   }
+
+//   if (tub) {
+//     console.log(num);
+//   }
+// }
+
+// v// ================================================================================================
+
+
+
+// let fo = 10
+
+// while (fo <= 20) {
+//   console.log(fo);
+//   fo++
+// }
+
+
+// do{
+//   console.log(fo);
+//   fo++
+// }while(f0 <= 20)
+
+
+// let status = "name"
+
+
+// let user = {
+
+// name:"eshmat",
+
+// status:"web develober",
+
+// [status]: "eshmatjon",
+
+// status
+// }
+
+// console.log(user[status]);
+// console.log(user.status);
+// console.log(user.name);
+
+
+
+
+
+// console.log("oxford" || true);
+// console.log("oxford" && true);
+// console.log("oxford" ?? true);
+// console.log(null || "oxford");
+// console.log(null && "oxford");
+// console.log(null ?? "oxford");
+
+// let str = "Algoritm_6 and Oxford_2.!"
+// let newstr = str.toLowerCase()
+// let result = " ";
+// for (let value of newstr) {
+//   if (value.toUpperCase() !== value) || ()
+//      {
+
+//   }
+// }
+
+
+// let arr = [
+//     1,
+//     "salom",
+//     true,
+//     false,
+//     null,
+//     undefined,
+//     null,
+//     { name: "salom" }
+// ]
+
+// console.log(arr[arr.length - 1
+
+// ]);
+
+
+// for (let i = 0; i < arr.length; i++) {
+//    console.log(arr[i]);
+    
+// }
+
+// let a = 0
+// while (a < arr.length) {
+//     console.log(arr[a]);
+//     a++
+// }
+
+// do{
+//     console.log(arr[a]);
+//     a++
+// }while(a < arr.length)
+
+
+
+let a = -7.5678
+
+console.log(Math.floor(a));
+console.log(Math.trunc(a));
+console.log(Math.round(a));
+console.log(Math.sign(a));
+console.log(parseInt(a));
+console.log(a.toFixed(3));
