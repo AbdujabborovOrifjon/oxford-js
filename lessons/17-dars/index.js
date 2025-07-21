8133b0b1791d123cd2ceb209e7d87674fe56e2af
AbdujabborovOrifjon <abdujabborovorifjon0@gmail.com>
2025-07-21 21:11:01 +0500

    change

---
+++ lessons/17-dars/index.js
@@ -0,0 +1,41 @@
+
+
+// let arr2 = arr.forEach((item)=> {
+// console.log(item);
+
+// })
+
+// for (let a = 0; a <= 10; a++) {
+//  if (a % 2 === 0) {
+//     console.log(a);
+    
+//  }    
+// }
+// let arr = [1, 2, 3, 4, 5]
+
+// let num = 0
+// for (let a = 0; a < arr.length; a++) {
+//     num += arr[a]
+// }
+// console.log(num);
+
+
+// let arr = [14, -8, 3, -7, 0, 9, -1]
+
+   
+// for (let a = 0; a < arr.length; a++) {
+// if (arr[a] > 0) {
+//     console.log(arr[a]);
+    
+// }    
+// }
+// for (let a = -10; a <= -1; a++) {
+//  console.log(a);
+    
+// }
+
+
+
+let arr = [1,2,3,4,5,6,7,8,9,10]
+
+let 
