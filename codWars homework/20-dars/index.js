3c74f5965e4c88e73e65218db2a9028e9ad39b9a
AbdujabborovOrifjon <abdujabborovorifjon0@gmail.com>
2025-07-26 10:32:41 +0500

    cod

---
+++ codWars homework/20-dars/index.js
@@ -0,0 +1,29 @@
+// 1 Savol javob)
+            function get(str){
+          return str.split("")
+            }
+// 2 savol javobi 
+
+function invert(array) {
+  return array.map(num => -num);
+}
+
+// 3 savol javobi
+
+function abbrevName(name) {
+  let parts = name.split(" ");
+  return parts[0][0].toUpperCase() + "." + parts[1][0].toUpperCase();}
+
+
+// 4 savol javobi
+
+function makeUpperCase(str) {
+return str.toUpperCase();
+}
+
+
+// 5 savol javobi
+
+function greet(name) {
+  return `Hello, ${name} how are you doing today?`;
+}
--- lessons/19-dars/index.js
+++ lessons/19-dars/index.js
@@ -1,10 +1,43 @@
 // distructururing
 
-// array example - arraydagi elementlarni olib ishlatish usuli
-let arr = [10, "john", true, "22"]
+// // array example - arraydagi elementlarni olib ishlatish usuli
+// let arr = [10, "john", true, "22"]
 
-// ... - spreed operator arraydagi nusxa olish uchun 
+// // ... - spreed operator arraydagi nusxa olish uchun 
 
-let [ten, booleanTrue, ...hammasi] = arr;
+// let [ten, booleanTrue, ...hammasi] = arr;
 
-console.log(hammasi);
+// console.log(hammasi);
+
+
+// let obj = {
+//   name:"eshmat",
+//   age:20,
+//   status:"talaba",
+// }
+
+
+// // // ... - spreed operator objectlarda nusxa olish uchun 
+// // object distrucing - object dagi qiymatlarni ozimiz nomlab  olish yoki objectdagi qiymatni olish nusxa olish usuli
+
+// let {age, ...hammmasi} = obj
+
+// console.log(hammmasi);
+
+// nested disctrucning
+// let arr = [1, [2, [5]]]
+// let [bir, [ikki, [uchun]]] = arr
+
+// console.log(uchun);
+
+// DOM - document ogject modul - htmlda daraxt misolidagi tuzulma
+// let text = document.querySelector(".text");
+
+// text.style.color = "blue  "
+
+// console.log(text);
+
+
+function removespace(params) {
+  
+}
